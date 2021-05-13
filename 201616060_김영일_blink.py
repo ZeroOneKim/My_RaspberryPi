5d166bcf75cdbfb5c1fa65ebbf1dc993a90212ea
ZeroOneKim <83469080+ZeroOneKim@users.noreply.github.com>
2021-05-13 17:24:49 +0900

    Add files via upload

---
+++ 201616060_김영일_blink.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+LedPin = 11
+def setup():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(LedPin, GPIO.OUT)
+	GPIO.output(LedPin, GPIO.HIGH)
+def loop():
+	while True:
+		print("...led on")
+		GPIO.output(LedPin, GPIO.LOW)
+		time.sleep(0.5)
+		print("led off...")
+		GPIO.output(LedPin, GPIO.HIGH)
+		time.sleep(0.5)
+def destroy():
+	GPIO.output(LedPin, GPIO.HIGH)
+	GPIO.cleanup()
+if__name__ == '__main__':
+	setup()
+	try:
+		loop()
+	except Keyboardinterrupt:
+		destroy()
